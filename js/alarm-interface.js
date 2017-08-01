ca5097e4dda9c322984fade551c04a9734f61110
Calla Rudolph and jacob ruleaux <jacob@>
2017-08-01 10:35:13 -0700

    adjust logic for display of alarm

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,11 +1,20 @@
 var Alarm = require('./../js/alarm.js').alarmModule;
 
 $(document).ready(function() {
-  $('#time').text(moment());
+  function update() {
+    $('#clock').html(moment().format('H:mm A'));
+  }
+  setInterval(update, 1000);
   $('#alarm').hide();
+  var time = moment().format("H:mm");
   $('#alarm-form').submit(function(event) {
     event.preventDefault();
-    var time = $('#set').val();
-    $('#alarm').show();
+    var alarmSet = $('#set').val();
+    var alarm = new Alarm(alarmSet);
+    var winning = alarm.clock(time);
+    console.log(winning);
+    if (winning === 1) {
+      $('#alarm').show();
+    } 
   });
 });
--- js/alarm.js
+++ js/alarm.js
@@ -1,7 +1,15 @@
-function Alarm() {
+function Alarm(alarmSet) {
+  this.alarmSet = alarmSet;
 }
 
-Alarm.prototype.set = function(time) {
+Alarm.prototype.clock = function(time) {
+  var test = 0;
+  if (this.alarmSet > time) {
+    test = 1;
+  } 
+  return test;
 };
+
+
 
 exports.alarmModule = Alarm;
