a3f36711999093d39a42a19be0bddb16e494f8a7
Calla Rudolph and jacob ruleaux <jacob@>
2017-08-01 15:16:43 -0700

    add setInterval conditional to interface to show alarm

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -2,32 +2,23 @@ var Alarm = require('./../js/alarm.js').alarmModule;
 
 $(document).ready(function() {
   function update() {
-    $('#clock').html(moment().format('H:mm A'));
+    $('#clock').html(moment().format('h:mm A'));
   }
   setInterval(update, 1000);
   $('#alarm').hide();
-  var time = moment().format("H:mm");
-  // var test = moment().format("HH:mm:ss");
-  // console.log(test);
-  // console.log(time);
+  var time = moment().format("h:mm");
 
   $('#alarm-form').submit(function(event) {
     event.preventDefault();
     var alarmSet = $('#set').val();
     var alarm = new Alarm(alarmSet);
-    var winning = alarm.clock(time);
-    // var timer = alarm.timer();
+    // var winning = alarm.clock(test);
 
-    // function slowAlert() {
-    //   alert('hi');
-    // }
-    //
-    // function delayedAlert() {
-    //   timeoutID = window.setTimeout(slowAlert, alarmSet);
-    // }
-
-    if (winning === 1) {
-      $('#alarm').show();
-    }
+    setInterval(function(){
+      // alarm.clock();
+      if (alarm.clock() === true) {
+        $('#alarm').show();
+      }
+    }, 1000);
   });
 });
--- js/alarm.js
+++ js/alarm.js
@@ -1,22 +1,16 @@
 function Alarm(alarmSet) {
   this.alarmSet = alarmSet;
-  // this.time = time;
 }
 
-Alarm.prototype.clock = function(time) {
-  var test = 0;
-  if (this.alarmSet > time) {
-    test = 1;
+
+Alarm.prototype.clock = function() {
+  var time = moment().hour() + ":" + moment().minute();
+  if (this.alarmSet === time) {
+    return true;
+  } else {
+    return false;
   }
-  return test;
 };
 
-// Alarm.prototype.timer = function() {
-//   this.time = function update() {
-//     $('#clock').html(moment().format('H:mm A'));
-//   }
-//   setInterval(this.time, 1000);
-//   return this.time;
-// }
 
 exports.alarmModule = Alarm;
